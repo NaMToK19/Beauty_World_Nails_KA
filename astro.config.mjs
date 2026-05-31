9395da3a57edfab5500bed87549d2df9f4884a16
NaMToK14 <anhviet.ngo@gmail.com>
2026-05-31 13:54:02 +0200

    Feat: GitHub Pages Deployment-Workflow und Base-Pfad hinzugefügt

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://NaMToK19.github.io',
+  base: '/Beauty_World_Nails_KA',
   vite: {
     plugins: [tailwindcss()]
   }
